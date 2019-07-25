024c2b0fef105a80998d630de51bd9706479ba50
Jasper Ginn <jasperginn@gmail.com>
2019-07-25 11:21:18 +0200

    Update processing script

--- postprocess.py
+++ postprocess.py
@@ -7,11 +7,10 @@ import syntok.segmenter as segmenter
 from lxml import html
 from lxml.html.clean import clean_html
 import argparse
+import re
 
-SNIP_LENGTH = 100
-UPPER_BOUND = 220
-LOWER_BOUND = 160
-MIN_SENTENCE_TOKENS = 10
+MIN_SENTENCE_TOKENS = 5
+MIN_PARAGRAPH_TOKENS = 10
 
 # options
 OUT_FILE = "WikiEssentials_L4.txt"
@@ -64,7 +63,7 @@ def get_children(xml):
 def process_file(input_file):
 
     """Read an input (html) file from disk and process to paragraph-size chunks"""
-    #input_file = "data/./Arts/Literature/The_Golden_Ass.html"
+    #input_file = "data/./Everyday_life/Sports_and_recreation/Stuffed_toy.html"
     # Create labels from file path
     labels = '+'.join(input_file.split("/")[2:4])
 
@@ -79,69 +78,26 @@ def process_file(input_file):
     paragraphs_from_html = '\n\n'.join(html2paragraph(file_as_string))
     paragraphs = segmenter.process(paragraphs_from_html)
 
-    # Cut into snippets of approximate size
-    final_paragraphs = []
-    snippet_hold = []
-    snippet_length_hold = 0
+    paragraphs_new = []
     for paragraph in paragraphs:
-        if snippet_length_hold < LOWER_BOUND:
-            snippet = snippet_hold
-            snippet_length = snippet_length_hold
-        else:
-            snippet = []
-            snippet_hold = []
-            snippet_length = 0
+        paragraph_new = []
         for sentence in paragraph:
             sentence_new = []
             for token in sentence:
                 sentence_new.append(token.spacing + token.value)
-            # Add sentence to snippet
-            sentence_length = len(sentence_new)
-            if sentence_length < MIN_SENTENCE_TOKENS: continue
-            snippet_length_updated = snippet_length + sentence_length
-            if snippet_length_updated < LOWER_BOUND:
-                snippet.append("".join(sentence_new))
-                snippet_length = snippet_length_updated
-                continue
-            if snippet_length_updated > UPPER_BOUND:
-                final_paragraphs.append("".join(snippet))
-                snippet = []
-                snippet.append("".join(sentence_new))
-                snippet_length = len(snippet)
-            else:
-                snippet.append("".join(sentence_new))
-                snippet_length = snippet_length_updated
-        # Add to hold
-        snippet_hold = snippet
-        snippet_length_hold = snippet_length
-    # (last snippet)
-    if len(snippet_hold) > 0:
-        final_paragraphs.append("".join(snippet))
+            # Join
+            sentence_new_joined = re.sub(r'\[[0-9]\]', '', "".join(sentence_new))
+            if not sentence_new_joined.endswith("Wikipedia") and len(sentence_new_joined.split(" ")) > MIN_SENTENCE_TOKENS: paragraph_new.append(sentence_new_joined)
+        # Paragraph must be > 5
+        if len(" ".join(paragraph_new).split(" ")) > MIN_PARAGRAPH_TOKENS: paragraphs_new.append("".join(paragraph_new))
 
     # Print
-    if len(final_paragraphs) > 0:
-        print("\t[==>] Snippets: [{}]\n\t[==>] Sample: {}\n\t[==>] Length: {}\n".format(len(final_paragraphs), final_paragraphs[0],
-                                                                                  len(final_paragraphs[0].split(" "))))
-
-    return(final_paragraphs, labels)
-
-    # Iterate
-    """
-    final_paragraphs = []
-    for paragraph in paragraphs:
-        paragraph_clean = []
-        for sentence in paragraph:
-            sentence_clean = []
-            for token in sentence:
-                sentence_clean.append(token.value)
-            # Minimum length of sentence
-            if len(sentence_clean) > 5: paragraph_clean.append(" ".join(sentence_clean))
-
-        if len(paragraph_clean) > 1: final_paragraphs.append(" ".join(paragraph_clean))
+    if len(paragraphs_new) > 0:
+        print("\t[==>] Snippets: [{}]\n\t[==>] Sample: {}\n\t[==>] Length: {}\n".format(len(paragraphs_new), paragraphs_new[0],
+                                                                                  len(paragraphs_new[0].split(" "))))
 
     # Return
-    return(final_paragraphs, labels)
-    """
+    return(paragraphs_new, labels)
 
 # Call
 
