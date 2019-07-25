00934fbaa68587ca3b33992bcabdb507f8c3cf5d
Jasper Ginn <jasperginn@gmail.com>
2019-07-25 09:23:41 +0200

    Now cutting snippets in pre-defined lengths

--- postprocess.py
+++ postprocess.py
@@ -8,6 +8,11 @@ from lxml import html
 from lxml.html.clean import clean_html
 import argparse
 
+SNIP_LENGTH = 100
+UPPER_BOUND = 220
+LOWER_BOUND = 160
+MIN_SENTENCE_TOKENS = 10
+
 # options
 OUT_FILE = "WikiEssentials_L4.txt"
 
@@ -54,16 +59,19 @@ def get_children(xml):
 
     """List children ignoring comments"""
 
-    return [e for e in xml.iterchildren() if not isinstance(e, html.HtmlComment)]  # noqa
+    return [e for e in xml.iterchildren() if not isinstance(e, html.HtmlComment)]
 
 def process_file(input_file):
 
     """Read an input (html) file from disk and process to paragraph-size chunks"""
-
+    #input_file = "data/./Arts/Literature/The_Golden_Ass.html"
     # Create labels from file path
     labels = '+'.join(input_file.split("/")[2:4])
 
     # Open input file
+    #with open(input_file, "r") as f:
+    #    file_as_string = f.read()
+
     with input_file.open() as f:
         file_as_string = f.read()
 
@@ -71,7 +79,54 @@ def process_file(input_file):
     paragraphs_from_html = '\n\n'.join(html2paragraph(file_as_string))
     paragraphs = segmenter.process(paragraphs_from_html)
 
+    # Cut into snippets of approximate size
+    final_paragraphs = []
+    snippet_hold = []
+    snippet_length_hold = 0
+    for paragraph in paragraphs:
+        if snippet_length_hold < LOWER_BOUND:
+            snippet = snippet_hold
+            snippet_length = snippet_length_hold
+        else:
+            snippet = []
+            snippet_hold = []
+            snippet_length = 0
+        for sentence in paragraph:
+            sentence_new = []
+            for token in sentence:
+                sentence_new.append(token.spacing + token.value)
+            # Add sentence to snippet
+            sentence_length = len(sentence_new)
+            if sentence_length < MIN_SENTENCE_TOKENS: continue
+            snippet_length_updated = snippet_length + sentence_length
+            if snippet_length_updated < LOWER_BOUND:
+                snippet.append("".join(sentence_new))
+                snippet_length = snippet_length_updated
+                continue
+            if snippet_length_updated > UPPER_BOUND:
+                final_paragraphs.append("".join(snippet))
+                snippet = []
+                snippet.append("".join(sentence_new))
+                snippet_length = len(snippet)
+            else:
+                snippet.append("".join(sentence_new))
+                snippet_length = snippet_length_updated
+        # Add to hold
+        snippet_hold = snippet
+        snippet_length_hold = snippet_length
+    # (last snippet)
+    if len(snippet_hold) > 0:
+        final_paragraphs.append("".join(snippet))
+
+    # Print
+    if len(final_paragraphs) > 0:
+        print("\t[==>] Snippets: [{}]\n\t[==>] Sample: {}\n\t[==>] Length: {}\n".format(len(final_paragraphs), final_paragraphs[0],
+                                                                                  len(final_paragraphs[0].split(" "))))
+
+    return(final_paragraphs, labels)
+
     # Iterate
+    """
     final_paragraphs = []
     for paragraph in paragraphs:
         paragraph_clean = []
@@ -86,6 +141,7 @@ def process_file(input_file):
 
     # Return
     return(final_paragraphs, labels)
+    """
 
 # Call
 
